ba9963951f394cea0eca265f71d51632b482ec92
Devan <devan.moylan@backyardlion.com>
2017-08-22 17:43:08 -0600

    Increase Rank function grabs nested elements, parses their values and then adds the value of the argument to the value.

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.getElementById('nested').querySelector('.target')
 }
+
+function increaseRankBy(n) {
+  const list = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < list.length; i++) {
+    let starting_value = parseInt(list[i].innerHTML);
+    list[i].innerHTML = (starting_value + n);
+  }
+  return list
+}
+
